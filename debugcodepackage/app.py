2aee17bfa8f00191c9b0177497fca3e76915e07c
CloudLining <ankur5yk@gmail.com>
2024-05-09 11:14:37 +0530

    first commit

---
+++ debugcodepackage/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+app1 = Flask(__name__, template_folder="templates")
+
+load_dotenv()
+
+@app1.route('/')
+def home():
+    print(os.getcwd())
+    print(os.environ['EMAIL'])
+    print(os.environ['FLASK_ENV'])
+    return "<h1>Welcome to Home - changes updated </h1>"
+
+
---
+++ run.py
@@ -0,0 +1,5 @@
+from debugcodepackage import app
+
+if "__name__"=='__main__':
+    print("success")
+    app.run()
